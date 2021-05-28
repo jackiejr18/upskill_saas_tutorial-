ec0bbb3a6d8a2a931b557af657fe4c0f35d57c82
Jackie Wysinger <jwysinger@gmail.com>
2021-05-28 01:40:15 +0000

    Added avatar column to Profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210522223318) do
+ActiveRecord::Schema.define(version: 20210528013618) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20210522223318) do
   end
 
   create_table "profiles", force: :cascade do |t|
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
